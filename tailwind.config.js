92461bf11473439e8970fddcf232f25aa5fc98de
irfanrmdni <irfanrmdni209@gmail.com>
2022-07-02 23:00:55 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            purple: "#3E44E1",
+            darkblue: "#252641",
+            orange: "#F48C06",
+            black: "#000000",
+            white: "#ffffff",
+            fadeOrange: "#FFF2E1",
+            transparent: "transparent",
+        },
+        extend: {
+            fontFamily: {
+                body: ["Poppins"]
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
